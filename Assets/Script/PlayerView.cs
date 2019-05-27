e1cc3fe7e54099b02ce2ee0d94f3a3df1770aca1
onewkub <37097081+onewkub@users.noreply.github.com>
2019-05-28 00:32:43 +0700

    Initial commit

---
+++ Assets/Script/PlayerView.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerView : MonoBehaviour
+{
+    public float mouseSemsitive;
+    public Transform PlayerBody;
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked; // Lock Currsor;
+        
+    }
+
+    private void CameraRotation()
+    {
+
+    }
+}
