5553e749cf425b48d0b53d44ec449685b7136756
onewkub <37097081+onewkub@users.noreply.github.com>
2019-05-29 01:27:57 +0700

    Add Last Position Player When Collected an Item.
    - Add Last Position Player When Collected an Item.
    - Add Pointer

---
+++ Assets/Script/RaycastPointer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastPointer : MonoBehaviour
+{
+    private string itemTag = "CollectionItem";
+    public Transform PlayerBody;
+    public EnermyScript enermy;
+    public float range = 20f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            SelectIt();
+        }       
+    }
+
+    void SelectIt()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(PlayerBody.transform.position, PlayerBody.transform.forward, out hit, range))
+        {
+
+            if (hit.transform.CompareTag(itemTag))
+            {
+                enermy.itemIsCollected();
+                Destroy(hit.transform.gameObject, 0.2f);
+            }
+        }
+        
+    }
+}
